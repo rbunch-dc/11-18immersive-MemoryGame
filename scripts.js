29a83cefd14d8eb9d0860aa2663141dd9baff4c2
Robert Bunch <rdbunch@gmail.com>
2018-12-17 15:20:31 -0500

    Add matched

--- scripts.js
+++ scripts.js
@@ -21,7 +21,8 @@ $(document).ready(()=>{
             }
             cards.push(`<img src="./images/monsters-${monsterNumber}.png" />`);
             cards.push(`<img src="./images/monsters-${monsterNumber}.png" />`);
-        }        
+        }
+        cards = shuffleDeck(cards);
         // console.log(cards)
         // init a var to store our html inside of
         let memoryHTML = '';
@@ -39,9 +40,55 @@ $(document).ready(()=>{
         console.log(memoryHTML);
         // I'm sorry Jonathan... I will teach them a better way later!
         $('.memory-game').html(memoryHTML);
+
+        // User just clicked on a card
         $('.card-holder').click(function(){
             $(this).addClass('flip');
+            let cardsUp = $('.flip');
+            // if cardUp has 2 elements, then this is 
+            // the 2nd card. compare.
+            if(cardsUp.length === 2){
+                const card1 = cardsUp[0];
+                const card2 = cardsUp[1];
+                if(card1.innerHTML == card2.innerHTML){
+                    // these cards match!
+                    // remove flip
+                    cardsUp.removeClass('flip');
+                    cardsUp.addClass('matched');
+                }else{
+                    // these are not a match because the HTML is dif
+                    // JS is too dang fast. We have to let the user
+                    // see the card before we flip it back.
+                    // setTimeout to the rescue!
+                    setTimeout(()=>{
+                        cardsUp.removeClass('flip');
+                    },2000);
+                }
+            }else{
+                // only one card up. Do nothing
+            }
         });
     });
 });
+
+
+function shuffleDeck(aDeckToBeShuffled){
+    // Loop. A lot. Like those machines in casinos that make 
+    // funny noises.
+    // When the loop (lots of times) is document, the array 
+    // (deck) will be shuffled
+    for(let i = 0; i < 1000000; i++){
+        let rand1 = Math.floor(Math.random() * aDeckToBeShuffled.length);
+        let rand2 = Math.floor(Math.random() * aDeckToBeShuffled.length);
+        // we need to switch aDeckToBeShuffled[rand1] with
+        // aDeckToBeShuffled[rand2].
+        // BUT, we have to save the value of one of them so
+        // we can keep it for later
+        let card1Defender = aDeckToBeShuffled[rand1];
+        aDeckToBeShuffled[rand1] = aDeckToBeShuffled[rand2]
+        aDeckToBeShuffled[rand2] = card1Defender;
+    }
+    // console.log(aDeckToBeShuffled);
+    return aDeckToBeShuffled
+}
 
